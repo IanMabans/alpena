6d3c36262b92fa79045324156a6c54aadcd356d2
Ian Mabans <kamauian614@gmail.com>
2023-05-11 15:34:36 +0300

    Shopping list

---
+++ shopping_list/lib/models/category.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  spices,
+  convenience,
+  hygiene,
+  other,
+  sweets,
+}
+
+class Category {
+  const Category(this.title, this.color);
+
+  final String title;
+  final Color color;
+}
